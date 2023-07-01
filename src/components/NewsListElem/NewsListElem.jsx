85ad7ccb826652de817cf622932a3626bf8455c1
1xtend <vlad.mezhevyi@gmail.com>
2023-07-01 09:12:02 +0300

    removed Container, added loader

--- src/components/NewsListElem/NewsListElem.jsx
+++ src/components/NewsListElem/NewsListElem.jsx
@@ -1,16 +1,24 @@
 import styles from './NewsListElem.module.css';
 
+import { Box } from '@mui/material';
+
 function NewsListElem({ story }) {
   return (
-    <li className={styles.li}>
-      <h2>
-        <a href={story.url} target="_blank" rel="noopener noreferrer">
-          {story.title}
-        </a>
-      </h2>
+    // <div className={styles.div}>
+    //   <h2>
+    //     <a href={story.url} target="_blank" rel="noopener noreferrer">
+    //       {story.title}
+    //     </a>
+    //   </h2>
 
-      <span>{story.author}</span>
-    </li>
+    //   <span>{story.author}</span>
+    // </div>
+
+    <Box
+      sx={{
+        backgroundColor: 'red',
+      }}
+    ></Box>
   );
 }
 
